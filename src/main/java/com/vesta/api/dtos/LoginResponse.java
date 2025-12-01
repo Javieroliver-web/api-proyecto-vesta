5be3c2fe5b67a762007e1373d19dee521a5dbc6d
Javieroliver-web <javip200555@gmail.com>
2025-12-01 11:35:47 +0100

    LoginResponse agregado

---
+++ src/main/java/com/vesta/api/dtos/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.vesta.api.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    private String token;
+    private String rol;
+    private String nombre;
+}
