eb5df5645b632c1ea16adeb3b05497477684697a
Javieroliver-web <javip200555@gmail.com>
2026-02-04 14:18:41 +0100

    Limpieza de codigo

--- update_tomcat_env_current.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo sed -i 's|Environment="API_URL=.*"|Environment="API_URL=http://vesta-web.duckdns.org/vesta-api"|' /etc/systemd/system/tomcat.service
-sudo sed -i 's|Environment="FRONTEND_URL=.*"|Environment="FRONTEND_URL=http://vesta-web.duckdns.org/vesta-web"|' /etc/systemd/system/tomcat.service
-sudo systemctl daemon-reload
-sudo systemctl restart tomcat
